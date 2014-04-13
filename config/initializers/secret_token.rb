587b582bb51b5598c436ca7b5a17c50962db3cd8
root <root@www.easy5ing.com>
2014-04-13 14:45:14 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Edingo::Application.config.secret_key_base = '3e69d44bbc89e309133fef1c6132e328aac4d6935b45eedbf50991e72a01c4f459e6926848b73ee9599674020e315b33637f76c0f255e9ad16f775b5a22bfc93'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Edingo::Application.config.session_store :cookie_store, key: '_edingo_session'
