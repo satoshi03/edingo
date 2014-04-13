7198fe809d0645eb3224f72365dc8341cda14f7a
taka@piplup <ororog@gmail.com>
2014-04-13 17:00:53 +0900

    moved to public and modified link

---
+++ public/editor.js
@@ -0,0 +1,53 @@
+// constants
+var UPDATE_INTERVAL = 2000;
+var BASEURL = '/documents/';
+
+var documentdata = '';
+
+$(function() {
+	var docid = $('#docid').val();
+	var docurl = BASEURL + docid + '.json';
+	var user = $('#username').val();
+	var update = false;
+
+	$.ajax({
+		url: docurl,
+		type: 'GET',
+		contentType: 'application/json; charset=utf-8'
+	}).done(function(response) {
+		$('#editarea').val(response.documents[0].content);
+	});
+
+	setInterval(function() {
+		$.ajax({
+			url: docurl,
+			type: 'GET',
+			contentType: 'application/json; charset=utf-8'
+		}).done(function(response) {
+			var storeddata = response.documents[0].content;
+			if (update) {
+				update = false;
+				var currentdata = $('#editarea').val();
+				var newdata = { documents: [{
+					id: docid,
+					last_edit_by: user,
+					content: currentdata
+				}]};
+				$.ajax({
+					url: docurl,
+					type: 'POST',
+					data: JSON.stringify(newdata)
+				}).done(function() {
+					console.log('newdata is saved');
+				});
+			} else {
+				$('#editarea').val(storeddata);
+			}
+		});
+	}, UPDATE_INTERVAL);
+
+	$('#editarea').keydown(function() {
+		update = true;
+		console.log('update');
+	});
+});
